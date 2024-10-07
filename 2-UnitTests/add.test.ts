317170b5bd8fbef3840b6600c1acf3f3e1c55378
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-10-07 19:55:24 +0200

    Bun Unit Tests - https://youtu.be/NSIITqbAO90?t=669

---
+++ 2-UnitTests/add.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it } from "bun:test";
+import {add} from "./index.ts";
+
+describe('add', () => {
+  it('should add two numbers', () => {
+    expect(add(1, 2)).toBe(3)
+    expect(add(1, 2)).toBeDefined()
+    expect(add(1, 2)).not.toBeArray()
+  })
+
+  it('should throw an error if a negative number is provided', () => {
+    expect(() => add(-1, 2)).toThrowError(/Negative NUMBERS/i)
+  })
+});
